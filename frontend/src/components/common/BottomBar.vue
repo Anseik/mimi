d7f2b24b7519635aab64d0fe83c0490e081bf35d
김유리 <youlee602@hanmail.net>
2021-03-23 11:11:24 +0900

    Revert "feat: add top bar and bottom bar"
    This reverts commit 2ae14b210d83f143f07f2257d44ba772eba38541.

--- frontend/src/components/common/BottomBar.vue
+++ frontend/src/components/common/BottomBar.vue
@@ -3,28 +3,22 @@
     :value="value"
     color="primary"
   >
-    <v-btn style="margin-top: 8px; margin-right: 3px">
+    <v-btn>
       <span>홈</span>
-      <i class="fas fa-home fa-3x"></i>
-      <!-- <v-icon>mdi-home</v-icon> -->
+
+      <v-icon>mdi-home</v-icon>
     </v-btn>
 
-    <v-btn style="margin-top: 8px; margin-left: 3px; margin-right: 3px">
+    <v-btn>
       <span>여행코스</span>
-      <i class="fas fa-suitcase-rolling fa-3x"></i>
-      <!-- <v-icon>mdi-bag-suitcase-outline</v-icon> -->
+
+      <v-icon>mdi-bag-suitcase-outline</v-icon>
     </v-btn>
 
-    <v-btn style="margin-top: 8px; margin-left: 3px; margin-right: 3px">
+    <v-btn>
       <span>타임라인</span>
-      <i class="fas fa-camera-retro fa-3x"></i>
-      <!-- <v-icon>mdi-account-clock-outline</v-icon> -->
-    </v-btn>
 
-    <v-btn style="margin-top: 8px; margin-left: 3px">
-      <span>다이어리</span>
-      <i class="far fa-calendar-alt fa-3x"></i>
-      <!-- <v-icon>event-outline</v-icon> -->
+      <v-icon>mdi-account-clock-outline</v-icon>
     </v-btn>
   </v-bottom-navigation>
 </template>
