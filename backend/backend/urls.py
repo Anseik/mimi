922a114c8bbcbcaed00cc35b0c08a0ebc2282c29
donggeolKim <ehd6481@naver.com>
2021-03-24 22:28:42 +0900

    build test 10:28

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -46,8 +46,8 @@ urlpatterns = [
     #api사양의 ReDoc보기
     #path("redoc/", schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     #api사양의 swagger-ui보기
-    path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name="schema-swagger-ui"),
-    path("admin/", admin.site.urls),
+    path("mimi/swagger/", schema_view.with_ui('swagger', cache_timeout=0), name="schema-swagger-ui"),
+    path("mimi/admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("member/", include(("member.urls", "member"))) #member 앱의 세부 url은 member앱의 urls에서 관리
 ]
