29074aeb76aa763ad1ffa9d0d73abb0cc2720727
anseik <kma9271@naver.com>
2021-03-25 09:56:05 +0900

    feat: add diary layout

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,21 +22,6 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-<<<<<<< HEAD
-#drf_yasg swagger정의 https://drf-yasg.readthedocs.io/en/stable/readme.html#openapi-3-0-note 공식문서참조
-schema_view = get_schema_view(
-    #info : Swagger API 정보 객체로써 생략시 기본값은 DEFAULT_INFO
-    openapi.Info(
-        title="Swagger mimi API",
-        default_version = "v1",
-        description="Swagger mimi API 문서",
-        terms_of_service="https://www.google.com/policies/terms/", 
-        contact=openapi.Contact(name="test", email="test@test.com"), 
-        license=openapi.License(name="Test License"), 
-    ), 
-    public=True, 
-    permission_classes=(permissions.AllowAny,), 
-=======
 # drf_yasg swagger정의 https://drf-yasg.readthedocs.io/en/stable/readme.html#openapi-3-0-note 공식문서참조
 schema_view = get_schema_view(
     # info : Swagger API 정보 객체로써 생략시 기본값은 DEFAULT_INFO
@@ -50,26 +35,16 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
->>>>>>> c62eeda0ece9a05640431984f4fb3037ac1f8012
 )
-
 
 
 # fmt: off
 urlpatterns = [
-<<<<<<< HEAD
-    #api사양의 JSON, YAML보기
-    #url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    #api사양의 ReDoc보기
-    #path("redoc/", schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    #api사양의 swagger-ui보기
-=======
     # api사양의 JSON, YAML보기
     #url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     # api사양의 ReDoc보기
     #path("redoc/", schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     # api사양의 swagger-ui보기
->>>>>>> c62eeda0ece9a05640431984f4fb3037ac1f8012
     path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
