73b3e49e1593bd051e636bce5fb7540b8e589495
DESKTOP-KVCQHCD\multicampus <adkgold77@naver.com>
2021-03-26 15:38:43 +0900

    update: recommend table

--- backend/recommendroad/models.py
+++ backend/recommendroad/models.py
@@ -9,13 +9,14 @@ class Store(models.Model):
     
     sid = models.CharField(max_length=30, primary_key=True)
     store_name = models.CharField(max_length=200)
-    branch = models.CharField(max_length=100)
-    area = models.CharField(max_length=100)
-    tel = models.CharField(max_length=100)
-    address = models.CharField(max_length=200)
-    latitude = models.CharField(max_length=100)
-    logitude = models.CharField(max_length=100)
-    category = models.CharField(max_length=100)
+    branch = models.CharField(max_length=100, null=True)
+    area = models.CharField(max_length=100, null=True)
+    tel = models.CharField(max_length=100, null=True)
+    address = models.CharField(max_length=200, null=True)
+    latitude = models.CharField(max_length=100, null=True)
+    logitude = models.CharField(max_length=100, null=True)
+    review_cnt = models.IntegerField(default=0)
+    category = models.CharField(max_length=100, null=True)
 
 
     class Meta:
@@ -26,8 +27,8 @@ class Menu(models.Model):
     
     mid = models.CharField(max_length=30, primary_key=True)
     sid = models.ForeignKey(Store, on_delete=models.CASCADE)
-    menu_name = models.CharField(max_length=100)
-    price = models.CharField(max_length=100)
+    menu_name = models.CharField(max_length=100, null=True)
+    price = models.CharField(max_length=100, null=True)
     
     class Meta:
         db_table = "menus"
@@ -38,7 +39,7 @@ class Review(models.Model):
     rid = models.CharField(max_length=30, primary_key=True)
     sid = models.ForeignKey(Store, on_delete=models.CASCADE)
     num = models.ForeignKey(Member, on_delete=models.CASCADE)
-    total_score = models.CharField(max_length=100)
+    total_score = models.CharField(max_length=100, null=True)
     content = models.TextField()
     reg_time = models.TextField()
 
@@ -51,14 +52,14 @@ class Landmark(models.Model):
     
     lid = models.CharField(max_length=30, primary_key=True)
     landmark_name = models.CharField(max_length=200)
-    addr = models.CharField(max_length=200)
-    latitude = models.CharField(max_length=200)
-    logitude = models.CharField(max_length=200)
-    facility = models.CharField(max_length=200)                    #편의시설정보 varchar(200)
-    park = models.CharField(max_length=200)                        #주차가능대수 varchar(200)
+    addr = models.CharField(max_length=200, null=True)
+    latitude = models.CharField(max_length=200, null=True)
+    logitude = models.CharField(max_length=200, null=True)
+    facility = models.CharField(max_length=200, null=True)                    #편의시설정보 varchar(200)
+    park = models.CharField(max_length=200, null=True)                        #주차가능대수 varchar(200)
     desc = models.TextField()                                      #관광지 소개 text
-    tel = models.CharField(max_length=200)
-    theme = models.CharField(max_length=200)
+    tel = models.CharField(max_length=200, null=True)
+    theme = models.CharField(max_length=200, null=True)
 
     class Meta:
         db_table = "landmarks"
@@ -76,12 +77,12 @@ class ZzimStore(models.Model):
 class ZzimStoreCourse(models.Model):
     
     num = models.ForeignKey(Member, on_delete=models.CASCADE)  #사용자구분번호 ( members.num )
-    sidB = models.ForeignKey(Store, on_delete=models.CASCADE)                        #아침코스(식당)  ( stores.sid )
-    isSavedB = models.CharField(max_length=100)                     #아침저장여부                 
-    sidL = models.ForeignKey(Store, on_delete=models.CASCADE)                       #점심코스(식당)  ( stores.sid )
-    isSavedL = models.CharField(max_length=100)                     #점심저장여부                 
-    sidD = models.ForeignKey(Store, on_delete=models.CASCADE)                        #저녁코스(식당)  ( stores.sid )
-    isSavedD = models.CharField(max_length=100)                     #저녁저장여부                 
+    sidB = models.ForeignKey(Store, on_delete=models.CASCADE, null=True)                        #아침코스(식당)  ( stores.sid )
+    isSavedB = models.CharField(max_length=100, null=True)                     #아침저장여부                 
+    sidL = models.ForeignKey(Store, on_delete=models.CASCADE, null=True)                       #점심코스(식당)  ( stores.sid )
+    isSavedL = models.CharField(max_length=100, null=True)                     #점심저장여부                 
+    sidD = models.ForeignKey(Store, on_delete=models.CASCADE, null=True)                        #저녁코스(식당)  ( stores.sid )
+    isSavedD = models.CharField(max_length=100, null=True)                     #저녁저장여부                 
 
     class Meta:
         db_table = "zzimStoreCourses"
@@ -90,12 +91,12 @@ class ZzimStoreCourse(models.Model):
 class ZzimLandCourse(models.Model):
     
     num = models.ForeignKey(Member, on_delete=models.CASCADE)  #사용자구분번호 ( members.num )
-    lid1 = models.ForeignKey(Landmark, on_delete=models.CASCADE)                #1코스
-    lid2 = models.ForeignKey(Landmark, on_delete=models.CASCADE)                #2코스
-    lid3 = models.ForeignKey(Landmark, on_delete=models.CASCADE)                #3코스
-    lid4 = models.ForeignKey(Landmark, on_delete=models.CASCADE)                #4코스
-    isSaved = models.CharField(max_length=100)                     #코스저장여부
-    savedDate = models.CharField(max_length=200)                   #코스 저장일자                   
+    lid1 = models.ForeignKey(Landmark, on_delete=models.CASCADE, null=True)                #1코스
+    lid2 = models.ForeignKey(Landmark, on_delete=models.CASCADE, null=True)                #2코스
+    lid3 = models.ForeignKey(Landmark, on_delete=models.CASCADE, null=True)                #3코스
+    lid4 = models.ForeignKey(Landmark, on_delete=models.CASCADE, null=True)                #4코스
+    isSaved = models.CharField(max_length=100, null=True)                     #코스저장여부
+    savedDate = models.CharField(max_length=200, null=True)                   #코스 저장일자                   
 
     class Meta:
         db_table = "zzimLandCourses"
